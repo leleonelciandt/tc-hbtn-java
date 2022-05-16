bdf38961a85a497ab1cf3fcab32668b31851329e
leleonelciandt <leticial@ciandt.com>
2022-05-16 16:03:26 -0300

    Correcting task 2

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,7 +1,7 @@
 public class Println {
     public static void main(String[] args) {
-        int num = 311;
-        System.out.print("Holberton School, %d", num);
+        int numero = 311;
+        System.out.print("Holberton School, %d", numero);
     }
 
 }

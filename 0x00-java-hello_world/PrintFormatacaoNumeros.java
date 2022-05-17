197c790c9d6d954a1d612a293fabc6e986304a00
leleonelciandt <leticial@ciandt.com>
2022-05-16 21:35:26 -0300

    Correcting task 6

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -5,7 +5,7 @@ public class PrintFormatacaoNumeros {
         float valor = 7654.321f;
 
         NumberFormat z = NumberFormat.getCurrencyInstance();
-        String percent = "%";
+        String percent = "% ";
 
         System.out.println("Valor: R" + z.format(valor));
         System.out.printf("Taxa: %.02f%s ", taxa, percent);

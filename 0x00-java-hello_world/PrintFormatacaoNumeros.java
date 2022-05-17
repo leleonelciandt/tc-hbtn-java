a7a28b59b4ab0f5fb34411eccdd83fda6842e340
leleonelciandt <leticial@ciandt.com>
2022-05-16 21:33:52 -0300

    Correcting task 6

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,16 +1,13 @@
+import java.text.NumberFormat;
 public class PrintFormatacaoNumeros {
-     import java.text.NumberFormat;
-     import java.util.Locale;
-
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
 
-        Locale localeUS = new Locale("en","US");
+        NumberFormat z = NumberFormat.getCurrencyInstance();
+        String percent = "%";
 
-        NumberFormat valorMoedaLocal = NumberFormat.getCurrencyInstance(localeUS);
-
-        System.out.println("Valor: R$ " + valor.format(valorMoedaLocal));
-        SYstem.out.println("Taxa: %.02f", taxa + "%" );
+        System.out.println("Valor: R" + z.format(valor));
+        System.out.printf("Taxa: %.02f%s ", taxa, percent);
     }
 }

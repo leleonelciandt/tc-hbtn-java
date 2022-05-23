fe699f160d47e7c0eb77db3676617d4fdc40e11e
leleonelciandt <leticial@ciandt.com>
2022-05-23 18:01:56 -0300

    Task 5 - Herança

---
+++ 0x05/abstrata/Quadrado.java
@@ -0,0 +1,23 @@
+public class Quadrado extends Retangulo {
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado <= 0){
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        } else {
+            this.altura = lado;
+            this.largura = lado;
+            this.lado = lado;
+        }
+    }
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %.02f", lado);
+    }
+
+}

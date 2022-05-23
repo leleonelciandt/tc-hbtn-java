b7853feb452cd35304818b6d6604ee5c7bea94de
leleonelciandt <leticial@ciandt.com>
2022-05-23 11:04:34 -0300

    Numeros 0 a 99

---
+++ 0x01/numeros/src/Numeros0a99.java
@@ -0,0 +1,10 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+
+        int num = 0;
+        while (num < 99) {
+            System.out.print(num++ + ", " );
+        }
+        System.out.print(num);
+    }
+}

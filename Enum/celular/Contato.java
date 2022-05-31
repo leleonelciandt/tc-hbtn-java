2e7043a492dfa82c3f2a57e3d51485cdd9e82585
leleonelciandt <leticial@ciandt.com>
2022-05-31 10:11:02 -0300

    Task 0 - Enum

---
+++ Enum/celular/Contato.java
@@ -0,0 +1,23 @@
+public class Contato {
+
+    private String nome, numeroTelefone;
+    private TipoNumero tipoNumero;
+
+    public Contato(String nome, String numeroTelefone, TipoNumero tipoNumero) {
+        this.nome = nome;
+        this.numeroTelefone = numeroTelefone;
+        this.tipoNumero = tipoNumero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public TipoNumero getTipoNumero() {
+        return tipoNumero;
+    }
+}

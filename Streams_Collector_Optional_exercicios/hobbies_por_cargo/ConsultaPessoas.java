e26d17759ff3d394ba4962b0d33e133e397d3845
leleonelciandt <leticial@ciandt.com>
2022-06-13 14:44:20 -0300

    Task 4 - Stream exercício de fixação

---
+++ Streams_Collector_Optional_exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> listPessoas){
+
+        Supplier<TreeMap<String, TreeSet<Pessoa>>> novaLista = () -> new TreeMap<>(Comparator.reverseOrder());
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, novaLista, Collectors.toCollection(TreeSet::new)));
+
+    }
+
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> listPessoas){
+
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> listPessoas){
+
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+
+
+    public static Map<String, Double> obterMediaSalarioPorCargo (List<Pessoa> listPessoas){
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+
+
+    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> listPessoas){
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.flatMapping(pessoa -> pessoa.getHobbies().stream(), Collectors.toCollection(TreeSet::new))));
+    }
+
+
+
+
+
+
+}

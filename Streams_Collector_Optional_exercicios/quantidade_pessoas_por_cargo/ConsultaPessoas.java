45f161654acf13a4ac53e41836697cbec15c43ca
leleonelciandt <leticial@ciandt.com>
2022-06-13 14:20:23 -0300

    Task 1 - Stream exercício de fixação

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> listPessoas){
+
+        Supplier<TreeMap<String, TreeSet<Pessoa>>> novaLista = () -> new TreeMap<>(Comparator.reverseOrder());
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, novaLista, Collectors.toCollection(TreeSet::new)));
+
+    }
+
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> listPessoas){
+
+        return listPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+    }
+
+
+
+
+
+}

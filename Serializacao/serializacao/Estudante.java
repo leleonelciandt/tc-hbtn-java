fd4c0f45100f5155f029488b5dfbb734e2e34c1f
leleonelciandt <leticial@ciandt.com>
2022-06-13 15:46:21 -0300

    Task 0 - Serialização

---
+++ Serializacao/serializacao/Estudante.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Estudante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int idade;
+    private String nome;
+    private transient String senha;
+
+    public Estudante(int idade, String nome, String senha) {
+        this.idade = idade;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudante { " + "nome='" + this.nome + "', idade='" + this.idade + "', senha='" + this.senha + "' }";
+    }
+
+}
---
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class SerializarEstudantes<Estudante> {
+    private String nomeArquivo;
+
+    public SerializarEstudantes(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void serializar(List<Estudante> estudantes) {
+        try (ObjectOutputStream objectOuput = new ObjectOutputStream(new FileOutputStream(this.nomeArquivo))){
+            objectOuput.writeObject(estudantes);
+        } catch(Exception e) {
+            System.out.println("Nao foi possivel serializar");
+        }
+    }
+
+    public List<Estudante> desserializar() {
+        try (ObjectInputStream objectInput = new ObjectInputStream(new FileInputStream(this.nomeArquivo))){
+            List<Estudante> listaEstudantes = (List<Estudante>) objectInput.readObject();
+            return listaEstudantes;
+        } catch(Exception e) {
+            System.out.println("Nao foi possivel desserializar");
+            return null;
+        }
+    }
+
+}

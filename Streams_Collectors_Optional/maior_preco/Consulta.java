80c4028de4b83b6827a48710d5e5107756233ea8
leleonelciandt <leticial@ciandt.com>
2022-06-13 11:26:01 -0300

    Task 1 - Stream

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido (Pedido pedido){
+
+        List<Produto> prodPedido = pedido.getProdutos();
+
+        return prodPedido.stream()
+                .filter(p -> p.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+
+        Optional<Produto> biggestPrice = produtos.stream()
+                .max(Comparator.comparingDouble(Produto::getPreco));
+        return biggestPrice.orElse(null);
+
+    }
+
+
+}
